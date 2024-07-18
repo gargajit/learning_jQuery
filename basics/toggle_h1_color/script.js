1ffebcd86c45bbcae947a94a4abdb07bb205a514
Ajit Garg <118595104+gargajit@users.noreply.github.com>
2024-07-18 20:30:56 +0530

    Update script.js

--- basics/toggle_h1_color/script.js
+++ basics/toggle_h1_color/script.js
@@ -1,3 +1,14 @@
+// ------------  jQuery  -------------------------
 $("h1").click(function() {
     $("h1").toggleClass("red-heading");
 });
+
+
+// --------------  JS  ----------------------------
+/*
+var headingColor = document.querySelector("h1");
+headingColor.addEventListener("click", function() {
+    headingColor.classList.toggle("red-heading");
+});
+*/
+// --------------  JS  ----------------------------
