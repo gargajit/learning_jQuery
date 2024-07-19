9823a4aeeb99d0847b5602980fd699d54b5c9326
Ajit Garg <118595104+gargajit@users.noreply.github.com>
2024-07-19 10:37:41 +0530

    Create script.js

---
+++ basics/adding_element/before/script.js
@@ -0,0 +1,4 @@
+// adding paragraph element before button using the before method.
+$("button").click(function(){
+    $("button").before("<p>This paragraph is added using before method of jQuery.<br> The paragraph element is added before button.</p>");
+});
