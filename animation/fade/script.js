7520fcfd0fcb1541a78b652f09564053c195cca8
Ajit Garg <118595104+gargajit@users.noreply.github.com>
2024-07-22 10:56:21 +0530

    Create script.js

---
+++ animation/fade/script.js
@@ -0,0 +1,11 @@
+$(".fadeOut").click(function() {
+    $(".myDiv").fadeOut();
+});
+
+$(".fadeIn").click(function() {
+    $(".myDiv").fadeIn();
+});
+
+$(".fadeToggle").click(function() {
+    $(".myDiv").fadeToggle();
+});
