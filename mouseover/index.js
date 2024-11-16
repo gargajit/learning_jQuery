eb1d3079e7f56f1f393599d5609dbbb07bbcc1ec
Ajit Garg <118595104+gargajit@users.noreply.github.com>
2024-11-16 16:47:36 +0530

    Update index.js

--- mouseover/index.js
+++ mouseover/index.js
@@ -1,5 +1,5 @@
 function randColor() {
-    var colors = ["purple", "orange", "blue", "yellow", "pink", "black"];
+    var colors = ["purple", "orange", "blue", "yellow", "pink", "black", "yellowgreen", "teal", "cadetblue, "chocolate", "deeppink", "deepskyblue"];
     var colorNumber = getRandomNumber(colors.length);
     return colors[colorNumber];
 }
