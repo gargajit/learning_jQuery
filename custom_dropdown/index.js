8ab161640bc419d07bb3e3dc79a53897fdb368a3
Ajit Garg <118595104+gargajit@users.noreply.github.com>
2024-11-18 21:41:13 +0530

    Create index.js

---
+++ custom_dropdown/index.js
@@ -0,0 +1,33 @@
+$(".customButton").on("click", function() {
+        $(".options").slideToggle("fast");
+});
+
+$("td").on("mouseenter", function() {
+    $(this).addClass("highlight");
+})
+$("td").on("mouseleave", function() {
+    $(this).removeClass("highlight");
+})
+
+$("td").on({
+    mouseenter: function() {
+        $(this).addClass("highlight");
+    },
+    mouseleave: function() {
+        $(this).removeClass("highlight");
+    },
+    click: function() {
+        const elementId = $(this).attr("id");
+        if (elementId === "one") {
+            alert("Great! Enjoy every second of it. You deserve it.");
+        } else if (elementId === "two") {
+            alert("Being happy helps you increase blood in the body.");
+        } else if (elementId === "three") {
+            alert("Are you a Robot?");
+        } else if (elementId === "four") {
+            alert("That means you have lived your day today.");
+        } else if (elementId === "five") {
+            alert("There's always a bright end to a low point. Don't let the situation affect you.");
+        }
+    }
+});
